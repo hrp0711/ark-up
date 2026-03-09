e42eb63032e459922746641b12a6135fef7b1480
hrp0711 <hrp0711@gmail.com>
2026-03-09 11:41:08 -0500

    fix navbar icon import

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -117,7 +117,7 @@ export function Navbar() {
             <img
               src="/logo.png"
               alt="ArkUp Logo"
-              className="h-10 w-auto"
+              className="h-14 w-auto"
             />
           </Link>
 
